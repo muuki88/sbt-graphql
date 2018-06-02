588295600ba0531791b4a96ec97db90a67a43114
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2018-06-02 18:11:39 +0200

    FIX #25 Add more documentation and a test for nested types

---
+++ src/test/resources/apollo/blog/AddArticle.scala
@@ -0,0 +1,18 @@
+import com.example.GraphQLQuery
+import sangria.macros._
+object AddArticle {
+  object addArticle extends GraphQLQuery {
+    val Document = graphql"""mutation addArticle($$content: ArticleContent!) {
+  addArticle(content: $$content) {
+    id
+    title
+  }
+}"""
+    case class Variables(content: ArticleContent)
+    case class Data(addArticle: AddArticle)
+    case class AddArticle(id: ID, title: String)
+  }
+  case class ArticleAuthor(id: ID)
+  case class ArticleContent(title: String, body: String, tags: Option[List[String]], author: ArticleAuthor)
+  type ID = String
+}
