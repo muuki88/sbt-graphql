3e30dcbdf5f3e0317ac88461b3a96df5b509f8b5
Binh Nguyen <ngbinh@gmail.com>
2018-09-14 23:15:45 +0700

    Document ScalametaUtils

--- src/main/scala/rocks/muki/graphql/codegen/ScalametaUtils.scala
+++ src/main/scala/rocks/muki/graphql/codegen/ScalametaUtils.scala
@@ -2,7 +2,12 @@ package rocks.muki.graphql.codegen
 
 import scala.meta._
 
+/**
+  * More robust way to parse [[Type.Ref]] and [[Term.Ref]] from String.
+  * More discussion: https://gitter.im/scalameta/scalameta?at=5b9ba14f8909f71f75d1b4bd
+  */
 object ScalametaUtils {
+
   def typeRefOf(typeTerm: String): Type.Ref = {
     typeTerm.parse[Type].get.asInstanceOf[Type.Ref]
   }
