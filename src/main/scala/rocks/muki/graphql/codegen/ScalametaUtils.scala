9038b44a12f54724ee4b007c2148dd2d261ee00f
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2018-11-08 15:32:30 +0100

    Fix #57 correctly create imports

--- src/main/scala/rocks/muki/graphql/codegen/ScalametaUtils.scala
+++ src/main/scala/rocks/muki/graphql/codegen/ScalametaUtils.scala
@@ -4,7 +4,8 @@ import scala.meta._
 
 /**
   * More robust way to parse [[Type.Ref]] and [[Term.Ref]] from String.
-  * More discussion: https://gitter.im/scalameta/scalameta?at=5b9ba14f8909f71f75d1b4bd
+  * @see More discussion: https://gitter.im/scalameta/scalameta?at=5b9ba14f8909f71f75d1b4bd
+  * @see https://astexplorer.net/#/gist/ec56167ffafb20cbd8d68f24a37043a9/677e43f3adb93db8513dbe4e2c868dd4f78df4b3
   */
 object ScalametaUtils {
 
@@ -36,5 +37,47 @@ object ScalametaUtils {
 
   def termRefOf(term: String): Term.Ref = {
     termRefOf(term.split('.'))
+  }
+
+  /**
+    * Takes a list of import declarations and generates a list of scalameta import statements.
+    *
+    * @example {{{
+    *    importer(List("java.time._", "org.apache._")
+    * }}}
+    *
+    * @param imports a list of packages, objects or classes to import
+    * @return a list of scalameta import statements
+    */
+  def imports(imports: List[String]): List[Import] = {
+    imports.map { x =>
+      val i = importer(x.split("\\.").toList)
+      q"import ..$i"
+    }
+  }
+  
+  private def importer(imports: List[String]): List[Importer] = {
+    val reversedImportNames = imports.init.map(Term.Name(_)).reverse
+    val select = if(reversedImportNames.length > 1) recursiveTermSelect(reversedImportNames) else reversedImportNames.head
+
+    if (imports.last == "_") {
+      Importer(select, List(Importee.Wildcard())) :: Nil
+    } else {
+      Importer(select, List(Importee.Name(Name.Indeterminate(imports.last)))) :: Nil
+    }
+  }
+
+  /**
+    * Create a recursive Tree.Select structure for scalameta imports.
+    * Note that the names must be in reversed order to build the correct tree.
+    *
+    * @param names to fold into a tree structure
+    * @return names folded into a Term.Select from left to right
+    */
+  private def recursiveTermSelect(names: List[Term.Name]): Term.Select = names match {
+    case Nil => throw new IllegalStateException("Cannot create an empty import")
+    case name :: Nil => throw new IllegalStateException(s"Cannot create a import tree for a single name: $name")
+    case name :: qual :: Nil => Term.Select(qual, name)
+    case name :: qualifiers => Term.Select(recursiveTermSelect(qualifiers), name)
   }
 }
---
+++ src/test/scala/rocks/muki/graphql/codegen/ScalametaUtilsSpec.scala
@@ -0,0 +1,38 @@
+package rocks.muki.graphql.codegen
+
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.meta._
+
+class ScalametaUtilsSpec extends  WordSpec with Matchers {
+
+  "The ScalametaUtils" should {
+
+    "generate valid imports" when {
+      "a single member is imported" in {
+        val imports = ScalametaUtils.imports("foo.bar.MyClass" :: Nil)
+        imports should have size 1
+        imports.head.show[Syntax] should be("import foo.bar.MyClass")
+      }
+
+      "a single member from a single package is imported" in {
+        val imports = ScalametaUtils.imports("foo.MyClass" :: Nil)
+        imports should have size 1
+        imports.head.show[Syntax] should be("import foo.MyClass")
+      }
+
+      "a wildcard import is used" in {
+        val imports = ScalametaUtils.imports("foo.bar._" :: Nil)
+        imports should have size 1
+        imports.head.show[Syntax] should be("import foo.bar._")
+      }
+
+      "a wildcard import from a single package is used" in {
+        val imports = ScalametaUtils.imports("foo._" :: Nil)
+        imports should have size 1
+        imports.head.show[Syntax] should be("import foo._")
+      }
+    }
+  }
+
+}
